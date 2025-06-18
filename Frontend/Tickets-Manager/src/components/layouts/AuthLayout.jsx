7a618768b4afb2584d358594b33305b4f893a1a3
Daniel D <daniel.elohi326@gmail.com>
2025-06-18 20:48:17 +0530

    Responsive Auth page

--- Frontend/Tickets-Manager/src/components/layouts/AuthLayout.jsx
+++ Frontend/Tickets-Manager/src/components/layouts/AuthLayout.jsx
@@ -3,12 +3,13 @@ import background from '../../assets/background.png'
 import mainLogo from '../../assets/brandName.png'
 
 const AuthLayout = ({ children }) => (
-    <div className='flex'>
-        <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <img src={mainLogo} alt='Brand logo' className='h-10' />
-            {children}
+    <div className='flex relative min-h-screen'>
+        <div className="absolute inset-0 md:hidden bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${background})` }} />
+        <div className='relative w-full md:w-[60vw] px-12 pt-8 pb-12 backdrop-blur-md bg-white/30 shadow-lg flex flex-col items-center justify-center min-h-screen md:min-h-0'>
+            <img src={mainLogo} alt='Brand logo' className='h-10 mb-0 fixed top-[10px] left-[10px]' />
+            <div className='w-full max-w-3xl mt-8'> {children} </div>
         </div>
-        <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-cover bg-no-repeat bg-center overflow-hidden p-8" style={{ backgroundImage: `url(${background})` }} />
+        <div className="hidden md:block w-[40vw] h-screen bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${background})` }} />
     </div>
 )
 
